6ea5a4e1a06fc0ee13e68cfc9a7b4c256d6960f0
Filip <flipchan@riseup.net>
2025-04-18 00:25:04 +0200

    fix clone

--- src/poseidon/pow5.rs
+++ src/poseidon/pow5.rs
@@ -17,7 +17,7 @@ use super::{
 use crate::utilities::Var;
 
 /// Configuration for a [`Pow5Chip`].
-#[derive(Clone, Debug, Clone)]
+#[derive(Clone, Debug)]
 pub struct Pow5Config<F: Field, const WIDTH: usize, const RATE: usize> {
     pub(crate) state: [Column<Advice>; WIDTH],
     partial_sbox: Column<Advice>,
